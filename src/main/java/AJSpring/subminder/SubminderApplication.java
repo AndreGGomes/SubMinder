652d7b996c51fd9b0393a451b038f7ab942a0d54
André Gomes <andrelucasrngomes@gmail.com>
2026-09-24 19:18:00 -0300

    issue-15

---
+++ src/main/java/AJSpring/subminder/SubminderApplication.java
@@ -0,0 +1,13 @@
+package AJSpring.subminder;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SubminderApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SubminderApplication.class, args);
+	}
+
+}
